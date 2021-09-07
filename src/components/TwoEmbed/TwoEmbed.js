67e6168c72f6fb155b4b1aa8e28e638a585801f5
unknown <jamienichols@codingbutter.com>
2021-09-06 23:33:31 -0400

    Update Twoembed

--- src/components/TwoEmbed/TwoEmbed.js
+++ src/components/TwoEmbed/TwoEmbed.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react";
+import { Icon } from "../global";
 import { EmbedFrame } from "./TwoEmbed.elements";
 const TwoEmbed = ({ tmdb, mediaType, season, episode }) => {
   const [source, updateSource] = useState([]);
+  const [showFrame, setShowFrame] = useState(false);
   useEffect(() => {
     updateSource(
       `https://www.2embed.ru/embed/tmdb/${
@@ -11,7 +13,13 @@ const TwoEmbed = ({ tmdb, mediaType, season, episode }) => {
     );
   }, [tmdb, mediaType, season, episode]);
 
-  return <EmbedFrame src={source} />;
+  return (
+    <>
+      {(showFrame && <EmbedFrame src={source} />) || (
+        <Icon iconName={"movies"} />
+      )}
+    </>
+  );
 };
 
 export default TwoEmbed;
