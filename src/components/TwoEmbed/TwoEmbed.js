3620da8256e20e3118a13211c0d4f5fc18f9c1ad
unknown <jamienichols@codingbutter.com>
2021-09-06 23:56:44 -0400

    Fixed conflict

--- src/components/TwoEmbed/TwoEmbed.js
+++ src/components/TwoEmbed/TwoEmbed.js
@@ -3,11 +3,8 @@ import { Icon } from "../global";
 import { EmbedFrame } from "./TwoEmbed.elements";
 const TwoEmbed = ({ tmdb, mediaType, season, episode }) => {
   const [source, updateSource] = useState([]);
-<<<<<<< HEAD
-  const [showFrame, setShowFrame] = useState(false);
-=======
+
   const [showPlayer, setShowPlayer] = useState(false);
->>>>>>> development
   useEffect(() => {
     updateSource(
       `https://www.2embed.ru/embed/tmdb/${
@@ -16,18 +13,13 @@ const TwoEmbed = ({ tmdb, mediaType, season, episode }) => {
       //"https://google.com"
     );
   }, [tmdb, mediaType, season, episode]);
-
-<<<<<<< HEAD
   return (
     <>
-      {(showFrame && <EmbedFrame src={source} />) || (
+      {(showPlayer && <EmbedFrame allow="fullscreen" src={source} />) || (
         <Icon iconName={"movies"} />
       )}
     </>
   );
-=======
-  return showPlayer && <EmbedFrame allow="fullscreen" src={source} />;
->>>>>>> development
 };
 
 export default TwoEmbed;
